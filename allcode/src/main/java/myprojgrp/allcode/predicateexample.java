ab3002f42fb029ce33fc186ca119277072b85b80
100027371 <100027371@APL-5ZQPQF2.amer.consind.ge.com>
2019-11-25 10:08:04 +0530

    predicate,streams

--- allcode/src/main/java/myprojgrp/allcode/predicateexample.java
+++ allcode/src/main/java/myprojgrp/allcode/predicateexample.java
@@ -1,5 +1,8 @@
 package myprojgrp.allcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.Predicate;
 
 /**
@@ -8,27 +11,46 @@ import java.util.function.Predicate;
  */
 public class predicateexample 
 {
+	
+	static boolean pred( int n, Predicate<Integer>predi){
+		System.out.println(n);
+		return predi.test(n);
+	}
     public static void main( String[] args )
     {
+
+    	//simple predicate
+    	Predicate<Integer> testlessten= i->(i<10);
+    	System.out.println(testlessten.test(9));
+    	System.out.println(testlessten.test(11));
+
+    	//predicate chain
+    	Predicate<Integer> testgrtten= i->(i>10);
+    	Predicate<Integer> testlesshundred= i->(i<100);
+
+    	System.out.println(testgrtten.and(testlesshundred).test(99));
+    	System.out.println(testgrtten.and(testlesshundred).test(9));
+
+    	//negate
+    	System.out.println(testgrtten.and(testlesshundred).negate().test(99));
+    	System.out.println(pred (11,i->i>10));
+
+    	StringJoiner strjoiner= new StringJoiner(";");
+    	strjoiner.add("ddd").add("rr");
+    	System.out.println(strjoiner);
     	
     	
-      //simple predicate
-      Predicate<Integer> testlessten= i->(i<10);
-      System.out.println(testlessten.test(9));
-      System.out.println(testlessten.test(11));
-      
-      //predicate chain
-      Predicate<Integer> testgrtten= i->(i>10);
-      
-      Predicate<Integer> testlesshundred= i->(i<100);
-      
-      System.out.println(testgrtten.and(testlesshundred).test(99));
-      System.out.println(testgrtten.and(testlesshundred).test(9));
-      
-      //negate
-      System.out.println(testgrtten.and(testlesshundred).negate().test(99));
-      
-      
-      
+    	//Streams
+    	
+    	List <Integer> listint= new ArrayList<>();
+    	
+    	listint.add(1);
+    	listint.add(2);
+    	listint.add(11);
+    	listint.add(19);
+    	
+    	
+    	System.out.println(listint.stream().filter(i->i>10).mapToInt(i->i).sum());
+    	
     }
 }
